921dd241ebdcae44e81e6ca07798cacdc276a7dc
Yeremia <yeremia.djaja@gmail.com>
2019-05-07 21:51:15 -0400

    more work on the base controller

--- server/src/controllers/base.controller.js
+++ server/src/controllers/base.controller.js
@@ -33,6 +33,8 @@ class Controller {
     const queryLimit =
         (limit === undefined) || (parseInt(limit, 10) <= 0) ? 1 : limit;
 
+    // Try to make the query. If successful, the result
+    // will be returned in the response.
     try {
       const data = await ((page, limit) => {
         return this._model
@@ -41,8 +43,11 @@ class Controller {
             .limit(limit);
       })(queryPage, queryLimit);
 
+      // Get the total number of records
       const numTotalRecords = await this._model.count();
 
+      // Send a resonse with the data back if
+      // the query is successful
       res.status(200).json({
         'status': 'Success',
         'details': 'Query made',
@@ -50,6 +55,7 @@ class Controller {
           'meta': {
             'numRecordsReturned': data.length,
             'numTotalRecords': numTotalRecords,
+            // eslint-disable-next-line max-len
             'recordsFrom': data.length == 0 ? 0 : ((queryPage * queryLimit) - queryLimit) + 1,
             'recordsTo': data.length == 0 ? 0 : queryPage * queryLimit,
           },
@@ -57,6 +63,7 @@ class Controller {
         },
       });
     } catch (err) {
+      // Otherwise send a status code of 400 with the error.
       res.status(400).json({
         'status': 'Error',
         'details': err,
@@ -69,9 +76,39 @@ class Controller {
    * Function that retrieves a single record based on
    * the url request parameter 'id'
    * @param {Object} req is the request object
-   * @param {*} res is the response object
+   * @param {Object} res is the response object
    */
   async getOne(req, res) {
+    // Make a query to the database to find the item by the
+    // specified ID
+    try {
+      const data = await this._model.findById(req.params.id);
+
+      // Handle the case where there is no data returned
+      // from the query. aka: no records match that ID.
+      if (!data) {
+        res.status(401).json({
+          'status': 'Error',
+          'details': 'Record with ID not found',
+          'data': null,
+        });
+      } else {
+        // Otherwise, send a status code of 200 and the data
+        res.status(200).json({
+          'status': 'Success',
+          'details': 'Record found',
+          'data': data,
+        });
+      }
+    } catch (err) {
+      // Catch the error if one is thown and send a status code
+      // of 400 with the appropriate data.
+      res.status(400).json({
+        'status': 'error',
+        'details': err,
+        'data': null,
+      });
+    }
   }
 
   /**
@@ -81,6 +118,27 @@ class Controller {
    * @param {Object} res is the response object
    */
   async create(req, res) {
+    // Create a new model with the request body data
+    const data = new this._model(req.body);
+
+    // Try to save it (to the db).
+    try {
+      await data.save();
+
+      // Send a 201 response if successful
+      res.status(201).json({
+        'status': 'Success',
+        'details': 'Record created',
+        'data': req.body,
+      });
+    } catch (err) {
+      // Otherwise catch the error and send a 400 response
+      res.status(400).json({
+        'status': 'Error',
+        'details': err,
+        'data': req.body,
+      });
+    }
   }
 
   /**
