cef239e35688b9e7d02844ebbbb169838326bbb3
Yeremia <yeremia.djaja@gmail.com>
2019-05-12 23:53:26 -0400

    added eslint, started laying out components

---
+++ client/src/components/UI/Buttons.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+// Button Styles
+const PrimaryButtonStyles = {};
+const PrimaryIconButtonStyles = {};
+const SecondaryButtonStyles = {};
+const IconButtonStyles = {};
+
+// Primary button with text
+// Takes in argument via props for onClick event handler value,
+// text value, and is active (disabled)
+const PrimaryButton = (props) => {
+  return (
+    <button></button>
+  );
+};
+
+// Primary button with text and icons
+// Takes in argument onClick event handler value, text value,
+// icon, and is active (disabled)
+const PrimaryIconButton = (props) => {
+  return (
+    <button></button>
+  );
+};
+
+// Secondary button with only text.
+// Takes in argument onClick event handler value, text,
+// and is active (disabled)
+const SecondaryButton = (props) => {
+  return (
+    <button></button>
+  );
+};
+
+// Icon button with only icon
+// Takes in argument onClick event handler value, icon,
+// and is active (disabled)
+const IconButton = (props) => {
+  return (
+    <button></button>
+  );
+};
+
+export {PrimaryButton, PrimaryIconButton, SecondaryButton, IconButton};
+
