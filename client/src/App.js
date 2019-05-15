bf994d524195410d616401f8b662f7f9212809ef
Yeremia <yeremia.djaja@gmail.com>
2019-05-15 07:16:44 -0400

    FIxed AppFrame dependancy issue.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import AppFrame from './components/AppFrame/AppFrame';
 
 const App = () => {
   return (
-    <AppFrame />
+    <div></div>
   );
 };
 
