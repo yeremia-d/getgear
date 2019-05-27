06f4731e8945772dd07d921d187e7d386ceef1f7
Yeremia <yeremia.djaja@gmail.com>
2019-05-27 01:55:11 -0400

    Added comments for util functions

--- client/src/components/common/utils.jsx
+++ client/src/components/common/utils.jsx
@@ -1,3 +1,6 @@
+/**
+ * Common colors
+ */
 const colors = {
   primary: 'FFFFFF',
   secondary: 'FFFFFF',
@@ -5,12 +8,18 @@ const colors = {
 
 };
 
+/**
+ * Standard Screen Sizes
+ */
 const screenSizes = {
   desktop: 992,
   tablet: 768,
   mobile: 576,
 };
 
+/**
+ * media templates for styled components
+ */
 const mediaQuery = Object.keys(screenSizes).reduce((acc, label) => {
   acc[label] = (...args) => css`
     @media only screen and (max-width: ${screenSizes[label] / 16}em) {
